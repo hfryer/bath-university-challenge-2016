d9c72629916688100a2097a346cb51ae8ffc4dcc
Harry Fryer <harryfryer@me.com>
2016-10-25 10:50:31 +0100

    Code Cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
- import java.io.BufferedReader;
-        import java.io.FileWriter;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.net.URL;
- import java.nio.Buffer;
- import java.util.ArrayList;
- import java.util.List;
- import java.util.regex.Matcher;
- import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
- /** Takes the top 500 web address's from the alex website
- * Created by HarryFryer on 05/04/2016.
- */
 public class Main {
-    private FileWriter writer;
-
-    private ArrayList<String> returns = new ArrayList<String>();
-
-
 
     public static void main (String args[]) {
-        for(int x =0;x<20;x++){
+        for(int x =0;x<20;x++) {
             try {
                 BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-                String URL_to_check= input.readLine();
+                String URL_to_check = input.readLine();
                 URL oracle = new URL(URL_to_check);
-                //URL oracle = new URL("http://www.");
-                BufferedReader in = new BufferedReader (new InputStreamReader(oracle.openStream()));
-
-                ArrayList<String> result = new ArrayList<String>();
+                BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
                     Pattern TAG_REGEX1 = Pattern.compile("<title>(.+?)</title>");
@@ -39,15 +24,15 @@ public class Main {
                     Matcher matcher2 = TAG_REGEX2.matcher(inputLine);
                     Matcher matcher3 = TAG_REGEX3.matcher(inputLine);
                     while (matcher1.find()) {
-                        System.out.println("title = "+matcher1.group(1));
+                        System.out.println("title = " + matcher1.group(1));
                         break;
                     }
                     while (matcher2.find()) {
-                        System.out.println("meta description = "+matcher2.group(1));
+                        System.out.println("meta description = " + matcher2.group(1));
                         break;
                     }
                     while (matcher3.find()) {
-                        System.out.println("meta keywords = "+matcher3.group(1));
+                        System.out.println("meta keywords = " + matcher3.group(1));
                         break;
                     }
                 }
@@ -55,19 +40,9 @@ public class Main {
                 in.close();
 
             } catch (IOException e) {
+                return;
 
             }
         }
-        /*try {
-            writer= new FileWriter("routes.txt",false);
-            for (String aResult:returns) {
-                writer.write(aResult + " \n");
-
-            }
-            writer.close();
-
-        }catch (IOException Ignored) {
-        }*/
-
     }
 }
